6d57303fb9ab9d76efd4ad745df0dabb8fb6ddd1
jvsgs0 <111323425+jvsgs0@users.noreply.github.com>
2024-08-23 21:37:04 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function start(){
+    window.location.href = "http://127.0.0.1:5500/index.html";
+}
+
+function biografia(){
+    window.location.href = "http://127.0.0.1:5500/biografia.html";
+}
+
+function info(){
+    window.location.href = "http://127.0.0.1:5500/info.html";
+}
+
+function shows(){
+    window.location.href = "http://127.0.0.1:5500/shows.html";
+}
+
+function cadavolta(){
+    window.location.href = "https://www.youtube.com/watch?app=desktop&v=q2RMPC_Makg";
+}
+
+function porumminuto(){
+    window.location.href = "https://www.youtube.com/watch?app=desktop&v=r_yhREbppbI";
+}
+
+function passoudaconta(){
+    window.location.href = "https://www.youtube.com/watch?app=desktop&v=XH25Mid-2gk";
+}
+
+function porteamardms(){
+    window.location.href = "https://www.youtube.com/watch?app=desktop&v=ibQJ-pdNglc";
+}
+
+function sempreseuhomem(){
+    window.location.href = "https://www.youtube.com/watch?app=desktop&v=2MNmi-k251g";
+}
